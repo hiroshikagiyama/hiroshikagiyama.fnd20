2dbb613f5ac7b81e5c73646363d0c3935cc68083
hiroshikagiyama <143395175+hiroshikagiyama@users.noreply.github.com>
2023-10-06 10:12:48 +0900

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,87 @@
+const menuArray =
+[
+    {
+        no: 1,
+        img: "anko_output.png",
+        name: "あんパン",
+        price: 350,
+    },
+    {
+        no: 2,
+        img: "choco_melon_output.png",
+        name: "チョコメロンパン",
+        price: 350,
+    },
+    {
+        no: 3,
+        img: "melon_output.png",
+        name: "メロンパン",
+        price: 350,
+    },
+    {
+        no: 4,
+        img: "corn_cheese_output.png",
+        name: "コーンチーズ",
+        price: 350,
+    },
+    {
+        no: 5,
+        img: "ham_cheese_output.png",
+        name: "ハムマヨチーズ",
+        price: 350,
+    },
+    {
+        no: 6,
+        img: "maru_output.png",
+        name: "まるパン",
+        price: 350,
+    },
+    {
+        no: 7,
+        img: "kurumi_output.png",
+        name: "くるみパン",
+        price: 350,
+    },
+    {
+        no: 8,
+        img: "koppe_output.png",
+        name: "コッペパン",
+        price: 350,
+    },
+    {
+        no: 9,
+        img: "wiener_output.png",
+        name: "パリパリウインナー",
+        price: 350,
+    },
+    {
+        no: 10,
+        img: "bagel_output.png",
+        name: "ベーグル",
+        price: 350,
+    },
+    {
+        no: 11,
+        img: "sweet_potato_output.png",
+        name: "スイートポテトパン",
+        price: 350,
+    },
+    {
+        no: 12,
+        img: "white_bread_output.png",
+        name: "ミニ食パン",
+        price: 350,
+    },
+    {
+        no: 13,
+        img: "white_bread_output.png",
+        name: "クロワッサン",
+        price: 350,
+    },
+    {
+        no: 14,
+        img: "white_bread_output.png",
+        name: "クロワッサン",
+        price: 350,
+    }
+]
